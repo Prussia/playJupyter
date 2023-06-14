803b63a255bf42b12c6d1b80907284855926a34f
Prussia <prussia.hu@gmail.com>
2023-06-14 18:01:11 +0800

    Update start_jupyter_notebook.sh

--- start_jupyter_notebook.sh
+++ start_jupyter_notebook.sh
@@ -2,6 +2,6 @@ echo ${PWD}
 
 cd  ${PWD}
 
-docker run -it --rm -p 8888:8888 -v "${PWD}":/home/jovyan/work \
+docker run -it --rm -p 8888:8888 -v /Users/prussia/workspaces/jupyter_workspace/jupyter-notebook:/home/jovyan/work \
 --user root -e NB_GID=100 -e GEN_CERT=yes -e GRANT_SUDO=yes \
 prussia/scipy-notebook
