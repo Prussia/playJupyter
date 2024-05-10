e98db9eb7aac90ed90bb8765ccf2fe3e7467a4d1
Prussia <prussia.hu@gmail.com>
2024-05-10 18:13:13 +0800

    Create start_jupyter_notebook_git.sh

---
+++ start_jupyter_notebook_git.sh
@@ -0,0 +1,7 @@
+echo ${PWD}
+
+cd  ${PWD}
+
+docker run -it --rm -p 8888:8888 -v /Users/prussia/Documents/GitHub/git/playJupyter:/home/jovyan/work \
+--user root -e NB_GID=100 -e GEN_CERT=yes -e GRANT_SUDO=yes \
+prussia/scipy-notebook
